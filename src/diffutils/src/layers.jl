739cfee9476f77eacf66fe5a425fc1ecc99a0d36
flyjgh <doctorzwei@gmail.com>
2021-11-03 05:45:32 +0100

    nw

--- src/diffutils/src/layers.jl
+++ src/diffutils/src/layers.jl
@@ -1,30 +1,78 @@
-    using Random
-    using CUDA: CuArray
-    using Flux: @functor
+using Random
+using CUDA: CuArray
+using Flux: @functor
+# -------------------------------------------------------------------
+nfan() = 1, 1
+nfan(n) = 1, n
+nfan(out, in) = in, out
+nfan(dims::Tuple) = nfan(dims...)
+nfan(dims...) = prod(dims[1:end-2]) .* (dims[end-1], dims[end])  # Conv Kernel
+glorot_uniform(rng::AbstractRNG, dims...) = (rand(rng, Float32, dims...) .- 5f-1) .* sqrt(24f0 / sum(nfan(dims...)))
+glorot_uniform(dims...) = glorot_uniform(Random.GLOBAL_RNG, dims...)
+# -------------------------------------------------------------------
+"""
+less alloc and slightly faster than Flux.Dense for CuArrays.
+"""
+struct Densegpu{A}
+    W::CuArray{Float32, 2}
+    b::CuArray{Float32, 1}
+    σ::A
+end
+@functor Densegpu
 
-    # -------------------------------------------------------------------
-    nfan() = 1, 1
-    nfan(n) = 1, n
-    nfan(out, in) = in, out
-    nfan(dims::Tuple) = nfan(dims...)
-    nfan(dims...) = prod(dims[1:end-2]) .* (dims[end-1], dims[end])  # Conv Kernel
-    glorot_uniform(rng::AbstractRNG, dims...) = (rand(rng, Float32, dims...) .- 5f-1) .* sqrt(24f0 / sum(nfan(dims...)))
-    glorot_uniform(dims...) = glorot_uniform(Random.GLOBAL_RNG, dims...)
-    # -------------------------------------------------------------------
-    """
-    less alloc and slightly faster than Flux.Dense for CuArrays.
-    """
-    struct Densegpu{A}
-        W::CuArray{Float32, 2}
-        b::CuArray{Float32, 1}
-        σ::A
-    end
+function Densegpu(In, out, σ=identity)
+    Densegpu(glorot_uniform(out, In) |> CuArray, glorot_uniform(out) |> CuArray, σ)
+end
 
-    function Densegpu(in, out, σ=identity)
-        Densegpu(glorot_uniform(out, in) |> CuArray, glorot_uniform(out) |> CuArray, σ)
-    end
-    @functor Densegpu
+(a::Densegpu)(x::CuArray{Float32,N}) where N = a.σ.(a.W * x .+ a.b)
+(a::Densegpu{identity})(x::CuArray{Float32,N}) where N = a.W * x .+ a.b
 
-    (a::Densegpu)(x::CuArray{Float32,N}) where {Float32,N} = a.σ.(a.W * x .+ a.b)
-    (a::Densegpu{identity})(x::CuArray{Float32,N}) where {Float32,N} = a.σ(a.W * x .+ a.b)
-    # -------------------------------------------------------------------
+# -------------------------------------------------------------------
+struct Recurrent{T,M,N}
+    comb::T
+    state::M
+    ƒ::N
+end
+@functor Recurrent
+
+function Recurrent(comb, ƒ, size, gpu=false)
+    gpu ?
+        Recurrent(comb, randn(Float32, size...) |> CuArray, ƒ) :
+        Recurrent(comb, randn(Float32, size...), ƒ)
+end
+
+function (a::Recurrent)(x::T) where T
+    m = a.ƒ(a.comb(a.state, x))
+    a.state .= m
+    return m
+end
+# -------------------------------------------------------------------
+
+struct RecurrentLayer{T,M,N,P,A}
+    comb::T
+    state::M
+    W::N
+    b::P
+    σ::A
+end
+@functor Recurrent
+
+function Recurrent(In::Int, out::Int, comb, σ=identity, gpu=false)
+    gpu ?
+        Recurrent(comb, zeros(Float32, out) |> CuArray, glorot_uniform(out, In) |> CuArray, glorot_uniform(out) |> CuArray, σ) :
+        Recurrent(comb, zeros(Float32, out), glorot_uniform(out, In), glorot_uniform(out), σ)
+end
+
+function (a::Recurrent)(x::T) where T
+    X = a.comb(a.state, x)
+    m = a.σ.(a.W * X .+ a.b)
+    a.state .= m
+    return m
+end
+
+function (a::Recurrent{T,M,N,P,identity})(x::S) where {T,M,N,P,S}
+    X = a.comb(a.state, x)
+    m = a.W * X .+ a.b
+    a.state .= m
+    return m
+end
